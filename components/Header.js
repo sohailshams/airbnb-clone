a7ba22b8edb1734e786961d101f9d1d3feb36ac1
sohailshams <sohailshamas@yahoo.com>
2021-09-11 12:15:54 +0200

    Added media query to show Become a host from medium screen and updated height of menu icon and user icon

--- components/Header.js
+++ components/Header.js
@@ -19,18 +19,18 @@ function Header() {
       </div>
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
-          className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
+          className="pl-2 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
           type="text"
           placeholder="Start your search"
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
-      <div className="flex space-x-4 items-center justify-end text-gray-500">
-        <p>Become a host</p>
+      <div className="flex space-x-3 items-center justify-end text-gray-500">
+        <p className="hidden md:inline">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-          <MenuAlt1Icon className="h-6" />
-          <UserAddIcon className="h-6" />
+          <MenuAlt1Icon className="md:h-6 h-4" />
+          <UserAddIcon className="md:h-6 h-4" />
         </div>
       </div>
     </header>
