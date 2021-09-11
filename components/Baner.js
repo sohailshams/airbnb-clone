da8090a17ac82cf0cf712e75718311c684ea4dd3
sohailshams <sohailshamas@yahoo.com>
2021-09-11 13:43:12 +0200

    Added I'm flexible button and styled it - changed color and bg and added shadow and rounded cornors

--- components/Baner.js
+++ components/Baner.js
@@ -10,6 +10,9 @@ function Baner() {
       />
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full my-3 font-bold">
+          I'm flexible
+        </button>
       </div>
     </div>
   );
