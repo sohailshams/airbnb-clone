523c917d86148c0a1c8dc80343582747bf27a5ba
sohailshams <sohailshamas@yahoo.com>
2021-09-11 13:36:13 +0200

    Added heights for hero image on different screen sizes and added a text in the middle of the image

--- components/Baner.js
+++ components/Baner.js
@@ -2,12 +2,15 @@ import Image from 'next/image';
 
 function Baner() {
   return (
-    <div>
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
       <Image
         src="https://links.papareact.com/0fm"
         layout="fill"
         objectFit="cover"
       />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+      </div>
     </div>
   );
 }
