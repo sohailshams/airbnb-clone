a652284bd86126f6765b7ac6e1200884f378134f
sohailshams <sohailshamas@yahoo.com>
2021-09-11 13:20:29 +0200

    Created Baner.js component and added hero image

---
+++ components/Baner.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+function Baner() {
+  return (
+    <div>
+      <Image
+        src="https://links.papareact.com/0fm"
+        layout="fill"
+        objectFit="cover"
+      />
+    </div>
+  );
+}
+
+export default Baner;
