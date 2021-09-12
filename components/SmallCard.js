c4c57345a644e83888940ae25920ad503fbfe9d5
sohailshams <sohailshamas@yahoo.com>
2021-09-12 23:04:44 +0200

    Rendered images, location and distance

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+
 function SmallCard({ img, location, distance }) {
-  return <div></div>;
+  return (
+    <div>
+      <div className="relative h-16 w-16">
+        <Image src={img} layout="fill" className="rounded-lg" />
+      </div>
+      <div>
+        <h2>{location}</h2>
+        <h3>{distance}</h3>
+      </div>
+    </div>
+  );
 }
 
 export default SmallCard;
