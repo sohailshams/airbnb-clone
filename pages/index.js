937a8780daef8bed32b93863337321a648b1aadd
sohailshams <sohailshamas@yahoo.com>
2021-09-12 22:47:21 +0200

    Created an async function to fetch data - add SmallCard component and destructured the props

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import Baner from '../components/Baner';
 import Header from '../components/Header';
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
+export default function Home({ exploreData }) {
+  console.log(exploreData);
   return (
     <div className="">
       <Head>
@@ -15,8 +17,27 @@ export default function Home() {
       <main className="max-w-7xl max-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {exploreData?.map(({ img, distance, location }) => (
+            <SmallCard
+              key={img}
+              img={img}
+              distance={distance}
+              location={location}
+            />
+          ))}
         </section>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
