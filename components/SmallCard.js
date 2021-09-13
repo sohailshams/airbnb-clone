6be4d31be59d2e48e9600cb7b0d661a413071b51
sohailshams <sohailshamas@yahoo.com>
2021-09-13 23:04:23 +0200

    Updated distance text color and updated location font

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -7,8 +7,8 @@ function SmallCard({ img, location, distance }) {
         <Image src={img} layout="fill" className="rounded-lg" />
       </div>
       <div>
-        <h2>{location}</h2>
-        <h3>{distance}</h3>
+        <h2 className="font-semibold">{location}</h2>
+        <h3 className="text-gray-500"> {distance}</h3>
       </div>
     </div>
   );
