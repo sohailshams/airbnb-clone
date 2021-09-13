732912fd0a79cfed9788f560bae4d2fc823add42
sohailshams <sohailshamas@yahoo.com>
2021-09-13 23:13:10 +0200

    Added grid system to display different columns on different screen sizes

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,19 @@ export default function Home({ exploreData }) {
       <Header />
       <Baner />
 
-      <main className="max-w-7xl max-auto px-8 sm:px-16">
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          {exploreData?.map(({ img, distance, location }) => (
-            <SmallCard
-              key={img}
-              img={img}
-              distance={distance}
-              location={location}
-            />
-          ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </div>
