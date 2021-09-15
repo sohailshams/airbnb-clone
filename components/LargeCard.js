9555e4cadfb7e174557c8eb5198d4eeedb1e2afa
sohailshams <sohailshamas@yahoo.com>
2021-09-15 22:55:41 +0200

    Styled the image

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -2,9 +2,14 @@ import Image from 'next/image';
 
 function LargeCard({ img, title, description, buttonText }) {
   return (
-    <section>
+    <section className="relative py-6 cursor-pointer">
       <div className="relative h-96 min-w-[300]">
-        <Image src={img} layout="fill" objectFit="cover" />
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl"
+        />
       </div>
     </section>
   );
