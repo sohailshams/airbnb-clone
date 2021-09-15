65dddc83f88f1a0658d923672a6fbf6c0b29f8db
sohailshams <sohailshamas@yahoo.com>
2021-09-15 23:04:48 +0200

    Added title, description and a button and styled it

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -11,6 +11,13 @@ function LargeCard({ img, title, description, buttonText }) {
           className="rounded-2xl"
         />
       </div>
+      <div className="absolute top-32 left-12">
+        <h3 className="text-4xl mb-3 w-64">{title}</h3>
+        <p>{description}</p>
+        <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-3">
+          {buttonText}
+        </button>
+      </div>
     </section>
   );
 }
