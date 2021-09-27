e14a503cb95437fd63a4a084572cd2f5ae80b54c
sohailshams <sohailshamas@yahoo.com>
2021-09-27 10:27:40 +0200

    Added facebook, linkedin and github urls and changed text color

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,25 @@
 function Footer() {
   return (
     <div className="grid grid-cols-3 bg-gray-100 text-center h-20 items-center ">
-      <a className="font-bold" href="">
+      <a
+        className="font-bold text-red-400"
+        target="_blank"
+        href="https://www.facebook.com/sohail.252/"
+      >
         Facebook
       </a>
-      <a className="font-bold" href="">
+      <a
+        className="font-bold text-red-400"
+        target="_blank"
+        href="https://www.linkedin.com/in/muhammad-sohail-shams-56995126/"
+      >
         Linkedin
       </a>
-      <a className="font-bold" href="">
+      <a
+        className="font-bold text-red-400"
+        target="_blank"
+        href="https://github.com/sohailshams"
+      >
         Github
       </a>
     </div>
